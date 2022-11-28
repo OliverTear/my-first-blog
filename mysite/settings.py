a98cd7d9ef84676f413c4c14ce2dc099d3c9d4ec
OliverTear <s1rtti7211@gmail.com>
2022-11-28 22:32:01 +0900

    My Django Girls app, first commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -65,11 +65,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'builtins': [
-
-                'bootstrap4.templatetags.bootstrap4',   #追加
-
-            ],
         },
     },
 ]
